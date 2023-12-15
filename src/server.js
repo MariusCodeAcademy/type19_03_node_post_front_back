236b2df408a4ab56b3c18e53bdf71095ce84b141
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-15 08:37:24 +0200

    server code

--- src/server.js
+++ src/server.js
@@ -1,2 +1,14 @@
-'use strict';
-console.log('server.js file was loaded');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
+// Middleware
+
+// Routes
+
+// app Listen
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
